95302774bfb653c5f39d82b538c6b5440242d380
ryo <ta090972@icloud.com>
2024-03-08 14:50:04 +0900

    user_id削除：db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_02_141405) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_08_044533) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -18,7 +18,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_02_141405) do
     t.datetime "updated_at", null: false
     t.string "author"
     t.string "picture"
-    t.string "user_id"
   end
 
   create_table "users", force: :cascade do |t|
