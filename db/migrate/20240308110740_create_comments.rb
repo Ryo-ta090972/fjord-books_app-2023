e64ed4d6375c7a3c5d793b3807d458fdaf95df88
ryo <ta090972@icloud.com>
2024-03-26 14:37:01 +0900

    commentsテーブルのカラムのオプションを設定

--- db/migrate/20240308110740_create_comments.rb
+++ db/migrate/20240308110740_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.integer :user_id
-      t.references :commentable, polymorphic: true
+      t.integer :user_id, null: false
+      t.references :commentable, polymorphic: true, null: false
 
       t.timestamps
     end
