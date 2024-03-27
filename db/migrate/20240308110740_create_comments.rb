17df87b6147f7e36621abca1990041e72c042989
ryo <ta090972@icloud.com>
2024-03-27 14:51:45 +0900

    user_idをreferences型に変更・外部キーオプション追加：reports,comments,schema

--- db/migrate/20240308110740_create_comments.rb
+++ db/migrate/20240308110740_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.integer :user_id, null: false
+      t.references :user, null: false, foreign_key: true
       t.references :commentable, polymorphic: true, null: false
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -56,14 +56,16 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_08_110740) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "reports", force: :cascade do |t|
     t.string "title"
     t.text "content"
-    t.integer "user_id"
+    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reports_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -84,4 +86,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_08_110740) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "users"
+  add_foreign_key "reports", "users"
 end
