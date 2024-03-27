f4482a3878d83e53bc30f7b1d1ac1f4df7a736e5
ryo <ta090972@icloud.com>
2024-03-27 22:44:50 +0900

    モデルテスト実装：reportクラスのインスタントメソッドをテスト

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,23 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "editable?" do
+    target_user = User.new(email: 'test@example.com', name:'test')
+    report = Report.new(user: target_user)
+    assert report.editable?(target_user)
+  end
+
+  test "created_on" do
+    date = '2024-3-27'.to_date
+    report = Report.new(created_at: date)
+    assert_equal date, report.created_on
+  end
+
+  test "save_mentions" do
+    mentioning_report = Report.new(content: 'http://localhost:3000/reports/1')
+    mentioned_report = Report.new(id: 1)
+    mentioning_report.mentioning_reports.each do |report|
+      assert_equal mentioned_report, report
+    end
+  end
 end
