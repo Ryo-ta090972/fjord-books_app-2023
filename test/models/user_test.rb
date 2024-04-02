c1f01d1dab4bf97dfb26c769e42dda38729cc3e7
ryo <ta090972@icloud.com>
2024-04-02 19:11:16 +0900

    rubocopによる修正

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,8 +3,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  test "name_or_email" do
-    user = User.new(email: 'test@example.com', name:'')
+  test 'name_or_email' do
+    user = User.new(email: 'test@example.com', name: '')
     assert_equal 'test@example.com', user.name_or_email
 
     user.name = 'test'
