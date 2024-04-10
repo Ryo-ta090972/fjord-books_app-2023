302535f10ab2fe40c148e05ae76c1e2c55ad6398
ryo <ta090972@icloud.com>
2024-04-10 20:47:59 +0900

    日報新規登録、更新時のsave_mentionsテスト修正：report_test.rb

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -19,11 +19,26 @@ class ReportTest < ActiveSupport::TestCase
     assert_equal current_date, report.created_on
   end
 
-  test 'save_mentions' do
-    mentioning_report = Report.new(content: 'http://localhost:3000/reports/1')
-    mentioned_report = Report.new(id: 1)
+  test 'save_mentions when create report' do
+    mentioned_report_1 = reports(:mentioned_report_1)
+    mentioning_report = reports(:mentioning_report)
+    mentioning_report.save
+    assert_not mentioning_report.mentioning_reports.empty?
+
     mentioning_report.mentioning_reports.each do |report|
-      assert_equal mentioned_report, report
+      assert_equal mentioned_report_1, report
+    end
+  end
+
+  test 'save_mentions when update report' do
+    mentioned_report_2 = reports(:mentioned_report_2)
+    mentioning_report = reports(:mentioning_report)
+    new_content = 'http://localhost:3000/reports/2'
+    mentioning_report.update(content: new_content)
+    assert_not mentioning_report.mentioning_reports.empty?
+
+    mentioning_report.mentioning_reports.each do |report|
+      assert_equal mentioned_report_2, report
     end
   end
 end
